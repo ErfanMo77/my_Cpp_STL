f52711f40665440691cc224a962e0bdd9e1f648a
Erfan Momeni <erfanmo98@gmail.com>
2023-06-14 00:53:42 +0300

    Start vector class.

---
+++ my_Cpp_STL/main.cpp
@@ -0,0 +1,98 @@
+// main.cpp : This file contains the 'main' function. Program execution begins and ends there.
+
+
+#include <iostream>
+#include "vector.h"
+
+struct Point3 
+{
+    float x = 0.0f, y = 0.0f, z = 0.0f;
+
+    Point3(float scalar)
+        :x(scalar), y(scalar), z(scalar) {}
+
+    Point3(float x, float y, float z)
+        :x(x),y(y),z(z) {}
+
+    Point3(const Point3& other) 
+        :x(other.x), y(other.y), z(other.z) 
+    {
+        std::cout << "Point3 COPY!\n";
+    }
+
+    Point3(Point3&& other) noexcept
+        :x(other.x), y(other.y), z(other.z) 
+    {
+        std::cout << "point3 MOVE!\n";
+    }
+
+    Point3& operator=(const Point3& other)
+    {
+        std::cout << "Point3 COPY!\n";
+        x = other.x;
+        y = other.y;
+        z = other.z;
+        return *this;
+    }
+
+    Point3& operator=(Point3&& other)
+    {
+        std::cout << "point3 MOVE!\n";
+        x = other.x;
+        y = other.y;
+        z = other.z;
+        return *this;
+    }
+
+    ~Point3()
+    {
+        std::cout << "Point3 Destroy!\n";
+    }
+
+};
+
+template<typename T>
+void PrintVector(const vector<T>& vec)
+{
+    std::cout << "Vector:\n";
+    std::cout << vec << std::endl;
+}
+
+template<>
+void PrintVector(const vector<Point3>& vec)
+{
+    std::cout << "Vector:\n";
+    std::cout << "size : " << vec.size() << std::endl;
+
+    //TODO write iterators
+    for (size_t i = 0; i < vec.size(); i++) 
+    {
+        std::cout << vec[i].x << ", " << vec[i].y << ", " << vec[i].z << std::endl;
+    }
+    std::cout << "!-----------------------------------------------!\n";
+}
+
+
+int main()
+{
+    std::cout << "Hello STL!\n";
+    vector<Point3> points;
+
+
+    points.PushBack({ 40.0f });
+    points.PushBack({ 2.0f ,0.1f,0.22f});
+    points.PushBack({ 3.0f ,0.6f,0.55f });
+    PrintVector(points);
+    
+    points.PopBack();
+    points.PopBack();
+    PrintVector(points);
+    points.PopBack();
+
+    PrintVector(points);
+    points.PopBack();
+    PrintVector(points);
+    points.PushBack({ 2 });
+
+    PrintVector(points);
+}
---
+++ my_Cpp_STL/vector.h
@@ -0,0 +1,85 @@
+#pragma once
+
+
+template<typename T>
+class vector {
+
+public:
+	vector() 
+	{
+		ReAllocate(2);
+	}
+
+
+	void ReAllocate(size_t newCapacity)
+	{
+		T* newData = (T*)::operator new (newCapacity * sizeof(T));
+
+		if (newCapacity < m_LastData)
+			m_LastData = newCapacity;
+
+		for (size_t i = 0; i < m_LastData; i++)
+		{
+			newData[i] = std::move(m_Data[i]);
+		}
+
+		::operator delete(m_Data, m_Capacity * sizeof(T));
+		m_Data = newData;
+		m_Capacity = newCapacity;
+	}
+
+	void PushBack(const T& val)
+	{
+		if (m_LastData >= m_Capacity)
+			ReAllocate(m_Capacity + m_Capacity / 2);
+
+		m_Data[m_LastData] = val;
+		m_LastData++;
+	}
+
+	void PushBack(T&& val)
+	{
+		if (m_LastData >= m_Capacity)
+			ReAllocate(m_Capacity + m_Capacity / 2);
+
+		m_Data[m_LastData] = std::move(val);
+		m_LastData++;
+	}
+
+	void PopBack()
+	{
+		if (m_LastData > 0)
+		{
+			m_LastData--;
+			m_Data[m_LastData].~T();
+		}
+	}
+
+	size_t size() const
+	{
+		return m_LastData;
+	}
+
+	T& operator[](size_t index)
+	{
+		return m_Data[index];
+	}
+
+	const T& operator[](size_t index) const
+	{
+		return m_Data[index];
+	}
+
+	
+
+private:
+
+	// Data vector
+	T* m_Data = nullptr;
+	// Index of the last valid data in the vector
+	size_t m_LastData = 0;
+	// Size of the vector
+	size_t m_Capacity = 0;
+
+
+};
